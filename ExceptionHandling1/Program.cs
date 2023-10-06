40af44c85529c51ac551add60e2bc6a1debf731b
Ayush Soni <ayusoni86@gmail.com>
2023-10-06 14:30:35 +0530

    Added description of assignment

--- ExceptionHandling1/Program.cs
+++ ExceptionHandling1/Program.cs
@@ -1,45 +1,72 @@
-﻿using System;
+﻿using ExceptionHandling1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Voter custom Exception
+
+//Assignment 1: Custom Exception Implementation (Voter Eligibility)
+
+//Create a custom exception class called UnderageVoterException that inherits from the System.Exception class. This exception should be used to handle cases where an individual is underage to vote.
+
+//Assignment 2: Exception Handling in Voter Eligibility Check
+
+//Develop a voter eligibility check module within your application.
+
+//Implement custom exception handling to calculate an individual's age based on their birthdate and check if they are eligible to vote.
+
+//If the age is below the legal voting age (e.g., 18 years old), throw the UnderageVoterException.
+
+//Catch and handle this exception appropriately by displaying a user-friendly error message.
+
+//Assignment 3: Logging Underage Voter Exceptions
+
+//Extend your application to include a basic logging mechanism.
+
+//When the UnderageVoterException is thrown due to an underage voter, log a simple error message to a log file.
+
 namespace ExceptionHandling1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Enter your age : ");
-            int age =Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine());
             try
             {
                 canVote(age);
-            }catch (UnderageVoterException ex)
+            }
+            catch (UnderageVoterException ex)
             {
                 Console.WriteLine(ex);
             }
             Console.ReadLine();
         }
 
-
-        static void canVote(int age)
+        private static void canVote(int age)
         {
-            if (age < 18) {
-                throw new UnderageVoterException("You cannot vote. Please come after "+ (18 - age) + " years");
+            if (age < 18)
+            {
+                throw new UnderageVoterException("You cannot vote. Please come after " + (18 - age) + " years");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Welcome, you can Vote!");
             }
         }
     }
 
-    class UnderageVoterException : Exception
+    internal class UnderageVoterException : Exception
     {
-        public UnderageVoterException() : base("This is a custom UnderageVoterException") { }
-        public UnderageVoterException(string msg) : base(msg) { }
-       
+        public UnderageVoterException() : base("This is a custom UnderageVoterException")
+        {
+        }
+
+        public UnderageVoterException(string msg) : base(msg)
+        {
+        }
     }
-
-
 }
